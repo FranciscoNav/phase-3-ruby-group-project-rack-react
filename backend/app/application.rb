52b133d2d4b002edcda3c2ace621dc3247f0bc14
Francisco <fnaveira11@gmail.com>
2021-06-08 17:22:36 -0400

    added path to handle post request for Trip

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -1,12 +1,30 @@
+require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    # binding.pry
+
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
-
+    elsif req.path.match(/expenses/)
+    
+    elsif req.path.match(/trips/)
+      if req.env["REQUEST_METHOD"] == "POST"
+        input = JSON.parse(req.body.read)
+        trip = Trip.create(name: input["name"], country: input["country"], city: input["city"])
+        return [200, { 'Content-Type' => 'application/json' }, [ trip.to_json ]]
+      else
+        if req.path.split("/trips").length == 0 
+          return [200, { 'Content-Type' => 'application/json' }, [ Trip.all.to_json ]]
+        else
+          trip_id = req.path.split("/trips/").last
+          return [200, { 'Content-Type' => 'application/json' }, [ Trip.find_by(id: trip_id).to_json({:include => :expenses})]]
+        end
+      end
     else
       resp.write "Path Not Found"
 
@@ -16,3 +34,4 @@ class Application
   end
 
 end
+
--- backend/db/migrate/20210607173934_create_trips.rb
+++ backend/db/migrate/20210607173934_create_trips.rb
@@ -2,6 +2,8 @@ class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
       t.string :name
+      t.string :country
+      t.string :city
     end
   end
 end
